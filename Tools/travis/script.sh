bc74930d8d27bfda4b5ff2e246ae86455463f5b1
Andreas Josef Heil <andijh92@gmx.at>
2017-07-03 18:21:50 +0200

    added ci scripts

---
+++ Tools/travis/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [[ $TRAVIS_OS_NAME = linux ]]; then
+    mkdir -p My\ Projects/Torque3D/buildFiles/ubuntu
+    pushd My\ Projects/Torque3D/buildFiles/ubuntu
+    cmake ../../../.. -DTORQUE_APP_NAME=Torque3D $ADDITIONAL_CMAKE_ARGS
+    make
+    make install || true
+    pushd My\ Projects/Torque3D/game
+    ./Torque3D runTests.cs || true
+    popd
+    popd
+
+elif
+    [[ $TRAVIS_OS_NAME = osx ]]; then
+    mkdir -p My\ Projects/Torque3D/
+    pushd My\ Projects/Torque3D/
+    cmake ../../ -DTORQUE_APP_NAME=Torque3D $ADDITIONAL_CMAKE_ARGS
+    cmake --build . --target install
+    pushd My\ Projects/Torque3D/game
+    ./Torque3D runTests.cs || true
+    popd
+    popd
+fi
