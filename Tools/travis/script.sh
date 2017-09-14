88c554249ae46630539afc27547f316e7161d87e
Andreas Josef Heil <andijh92@gmx.at>
2017-09-14 10:27:59 +0200

    try travis mac os deployment

--- Tools/travis/script.sh
+++ Tools/travis/script.sh
@@ -5,6 +5,8 @@ if [[ $TRAVIS_OS_NAME = linux ]]; then
     mkdir -p My\ Projects/Torque3D/buildFiles/ubuntu
     pushd My\ Projects/Torque3D/buildFiles/ubuntu
     cmake ../../../.. -DTORQUE_APP_NAME=Torque3D $ADDITIONAL_CMAKE_ARGS
+    echo $CFLAGS
+    echo $LDFLAGS
     make
     make install || true
     pushd ../../game
@@ -17,8 +19,8 @@ elif
     mkdir -p My\ Projects/Torque3D/
     pushd My\ Projects/Torque3D/
     cmake ../../ -DTORQUE_APP_NAME=Torque3D $ADDITIONAL_CMAKE_ARGS
-    # cmake --build . --target install
-    cmake --build .
+    echo $CFLAGS
+    echo $LDFLAGS
     cmake --build . --target install
     pushd game
     ./Torque3D runTests.cs || true
